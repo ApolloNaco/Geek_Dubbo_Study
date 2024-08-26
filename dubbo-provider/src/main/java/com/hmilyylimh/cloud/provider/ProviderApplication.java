bb15fa03a83bfcee93276d9983939b3ac3cc26da
cgy <495696989@qq.com>
2024-08-26 21:49:12 +0800

    原来父pom中没有生命的maven依赖，子项目是import不到的

--- dubbo-provider/src/main/java/com/hmilyylimh/cloud/provider/ProviderApplication.java
+++ dubbo-provider/src/main/java/com/hmilyylimh/cloud/provider/ProviderApplication.java
@@ -1,8 +1,10 @@
 package com.hmilyylimh.cloud.provider;
 
 
+import org.apache.dubbo.container.Main;
+
 public class ProviderApplication {
     public static void main(String[] args){
-
+        Main.main(args);
     }
 }
