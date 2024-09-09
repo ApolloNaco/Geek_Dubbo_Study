f92b066da726743fcc82323a24fad01a190c8179
cgy <495696989@qq.com>
2024-09-09 22:26:08 +0800

    实现本地调用 consumer.xml里面的url要改成你自己的本地ip

--- dubbo-consumer/src/main/java/com/hmilyylimh/cloud/consumer/ConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/hmilyylimh/cloud/consumer/ConsumerApplication.java
@@ -1,17 +1,22 @@
 package com.hmilyylimh.cloud.consumer;
 
+import com.hmilyylimh.cloud.api.UserQueryFacade;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
 
 public class ConsumerApplication {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String configPath = "classpath*:META-INF/spring/*.xml";
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(configPath);
         ctx.refresh();
 
         // 接口调用
-
+        UserQueryFacade userQueryFacade = ctx.getBean(UserQueryFacade.class);
+        System.out.println(userQueryFacade.queryUser("cgy"));
         // 阻塞等待
+        System.in.read();
     }
 }
--- dubbo-provider/src/main/java/com/hmilyylimh/cloud/provider/ProviderApplication.java
+++ dubbo-provider/src/main/java/com/hmilyylimh/cloud/provider/ProviderApplication.java
@@ -3,11 +3,18 @@ package com.hmilyylimh.cloud.provider;
 
 
 import org.apache.dubbo.container.Main;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.CountDownLatch;
 
 
 public class ProviderApplication {
-    public static void main(String[] args){
-        Main.main(new String[]{"spring","log4j"});
+    public static void main(String[] args) throws InterruptedException {
+//        Main.main(new String[]{"spring","log4j"});
 //        Main.main(new String[]{});
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-provider.xml");
+        context.start();
+        new CountDownLatch(1).await();
     }
 }
